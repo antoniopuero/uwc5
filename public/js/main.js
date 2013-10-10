b2cbe47aec7a1494e2c13a8dd7eddceeec5e00dc
Anton Savchenko <antonio.puero@gmail.com>
2013-10-10 20:10:50 +0300

    Add validation to login and register forms, handle account requests

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,29 @@
 requirejs.config({
-  baseUrl: 'js',
-  paths: {
-    jquery: 'components/jquery/jquery',
-    angular: 'components/angular/angular',
-    angularBootstrap: 'components/angular-bootstrap/ui-bootstrap',
-    angularTpls: 'components/angular-bootstrap/ui-bootstrap-tpls',
-    bootstrap: 'components/bootstrap.css/js/bootstrap',
-    app: 'app/app',
-    controllers: 'app/controllers',
-    directives: 'app/directives',
-    filters: 'app/filters',
-    services: 'app/services'
-  },
-  shim: {
-    bootstrap: {
-      deps: ['jquery']
-    },
-    app: {
-      deps: ['jquery', 'bootstrap']
-    },
-    angularBootstrap: {
-      deps: ['angular']
-    }
-  },
-  urlArgs: "bust=" + (new Date()).getTime()
+	baseUrl: 'js',
+	paths: {
+		jquery: 'components/jquery/jquery',
+		validator: 'components/jquery.validation/jquery.validate',
+		bootstrap: 'components/bootstrap.css/js/bootstrap',
+		app: 'app/app',
+		account: 'app/account'
+	},
+	shim: {
+		bootstrap: {
+			deps: ['jquery']
+		},
+		app: {
+			deps: ['jquery', 'bootstrap']
+		},
+		account: {
+			deps: ['jquery', 'validator']
+		},
+		validator: {
+			deps: ['jquery']
+		}
+	},
+	urlArgs: "bust=" + (new Date()).getTime()
 });
 
-requirejs(['app'], function(app) {
-  return app.initialize();
+requirejs(['app'], function (app) {
+	return app.initialize();
 });
